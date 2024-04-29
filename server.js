819b7d7c3b6113e3def72f0e0b6ae5fc09aaf658
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-29 11:25:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//const db  = require('./utils/db.js');
+
+const app = express()
+
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        status:"hello",
+        message:"kunal pawar"
+    })
+})
+
+app.listen(process.env.PORT,()=>{
+     console.log('listening on port',process.env.PORT);
+})
---
+++ utils/db.js
@@ -0,0 +1,20 @@
+const mongoose  = require('mongoose');
+
+mongoose.connect('mongodb+srv://kunalpawar8319:kunalmain2018@e-com.qihktib.mongodb.net/user');
+
+const db = mongoose.connection;
+
+db.on('connected',()=>{
+    console.log('connected to Mongodb server');
+});
+
+db.on('error',(err)=>{
+    console.log('mongodb connection error',err);
+});
+
+db.on('disconnected',()=>{
+    console.log('mongodb server disconnected');
+});
+
+
+module.exports = db;
