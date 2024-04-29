8392ac6f8b4543c1de082c7010e46e6832d4e076
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-29 11:40:43 +0530

    first commit

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const app = express()
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
+// app.get('/',(req,res)=>{
+//     res.status(200).json({
+//         status:"hello",
+//         message:"kunal pawar"
+//     })
+// })
+
+app.get('/kunal',(req,res)=>{
     res.status(200).json({
         status:"hello",
         message:"kunal pawar"
     })
 })
-
 app.listen(process.env.PORT,()=>{
      console.log('listening on port',process.env.PORT);
 })
