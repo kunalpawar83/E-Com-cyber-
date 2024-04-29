65e9996ab2b12f14333fc4f7222dae4f584db6dc
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-29 11:38:59 +0530

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/login',(req,res)=>{
+app.get('/',(req,res)=>{
     res.status(200).json({
         status:"hello",
         message:"kunal pawar"
