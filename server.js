f2edbd6dfa7a34bf52020572e8e826d1e145e177
kunalpawar83 <kunalpawar8319@gmail.com>
2024-04-30 14:45:59 +0530

    first commi

--- server.js
+++ server.js
@@ -3,25 +3,14 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const { connectDB } =  require('./utils/db.js');
 const PORT = process.env.PORT;
+const userRoute = require('./routes/userRoute.js')
 
 const app = express()
 
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// app.get('/',(req,res)=>{
-//     res.status(200).json({
-//         status:"hello",
-//         message:"kunal pawar"
-//     })
-// })
-
-app.get('/login',(req,res)=>{
-    res.status(200).json({
-        status:"hello",
-        message:"kunal pawar"
-    })
-})
+app.use('/user',userRoute);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
