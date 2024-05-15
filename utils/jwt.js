da2a7932e150f728819acb19627b0c2153317ba2
kunalpawar83 <kunalpawar8319@gmail.com>
2024-05-15 13:19:22 +0530

    acess token

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,17 +5,21 @@ const key = "kunal_29/06/2003"
 
 const jwtAuthMiddleware  = ( req,res,next)=>{
     try{
+      
         const authHeader = req.headers['authorization'];
-            if (authHeader) {
-                const token = authHeader.split(' ')[1]; // Assuming the token is sent as "Bearer <token>"
-                jwt.verify(token, 'your_secret_key', (err, decoded) => {
-                    if (err) {
-                        return res.sendStatus(403); // Forbidden if token is invalid
-                    }
-            req.user = decoded; // Store the decoded token payload in req.user for future use
-        next(); // Continue to the next middleware or route handler
-    });
-  } 
+        const token = authHeader.split(' ')[1]; 
+
+        if(!token){
+         return res.status(403).json({
+            status:"fail",
+            error:"unauthorized"
+         })
+        }
+
+        const decoded = jwt.verify(token,key);
+        req.user = decoded;
+        next();
+
     }catch(err){
         console.log(err);
         res.status(400).json({
