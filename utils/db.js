febcca8ea49e5b8ca8c3cf287b673458882f075a
kunalpawar83 <kunalpawar8319@gmail.com>
2024-05-16 11:48:26 +0530

    database connect to cyber email

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://kunalpawarcyberinstant:kunalmain2018@e-com-cyberinstant.6gqwswc.mongodb.net/user');
+    await mongoose.connect('mongodb+srv://kunalpawarcyberinstant:kunalmain2018@cluster0.8g5x7yn.mongodb.net/user');
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
