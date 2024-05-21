3b76a5a811963214179e9d73c701ba4abc2daf8c
kunalpawar83 <kunalpawar8319@gmail.com>
2024-05-21 16:29:41 +0530

    add logger to see what is goning on aap

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const userRoute = require('./routes/userRoute.js')
 const productRoute = require('./routes/productRoute.js');
 const categoryRoute = require('./routes/categoryRoute.js');
 const ratingRoute = require('./routes/ratingRoute.js');
+const logger = require('./utils/logger.js');
 
 const app = express()
 
 app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
 
 app.use('/user',userRoute);
 app.use('/product',productRoute);
