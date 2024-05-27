11699440487165af5a74c53722438bb338b3b01a
kunalpawar83 <kunalpawar8319@gmail.com>
2024-05-27 15:57:42 +0530

    set to env dev and prod

--- controller/errorCont.js
+++ controller/errorCont.js
@@ -1,8 +1,26 @@
+const senderrordev =(err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        error:err,
+        message:err.message,
+        stack:err.stack
+       })
+};
+
+const senderrorprod = (err,res)=>{
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+       })
+}
+
 module.exports = (err,req,res,next)=>{
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
-    res.status(err.statusCode).json({
-     status:err.status,
-     message:err.message
-    })
+
+    if(process.env.NODE_ENV === 'development'){
+        senderrordev(err,res);
+    }else if(process.env.NODE_ENV === 'production'){
+        senderrorprod(err,res);
+    }
 }
