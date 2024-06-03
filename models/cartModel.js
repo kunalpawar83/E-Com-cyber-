c340cdf4000476f42f5aee44352aa2ea029d2a7a
kunalpawar83 <kunalpawar8319@gmail.com>
2024-06-03 15:50:34 +0530

    cart add with full code

--- models/cartModel.js
+++ models/cartModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const cartItemSchema = new mongoose.Schema({
     productId:{
-        type: [mongoose.Schema.Types.ObjectId],
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'Product'
     },
